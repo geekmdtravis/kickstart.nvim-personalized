48e2b629e557b57b36b937b01f93ec4beb9f57b1
geekmdtravis <travis@geekmd.io>
2025-05-27 19:42:39 -0700

    added bufremove to ensure can keep buffers in focus and not switch to neotree

--- lua/custom/plugins/motionutils/mini.lua
+++ lua/custom/plugins/motionutils/mini.lua
@@ -16,6 +16,10 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
+    require('mini.bufremove').setup()
+    vim.keymap.set('n', '<leader>bd', '<CMD>lua require("mini.bufremove").delete()<CR>', {
+      desc = 'Delete Buffer',
+    })
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
